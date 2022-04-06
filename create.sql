ad87f9618da0c0672c9c6c004712db91df281815
Uday-Mittal-8860 <91343270+Uday-Mi@users.noreply.github.com>
2022-04-06 16:01:36 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,34 +1,38 @@
+create schema Project;
+
+use Project;
+
 create table course (
-    course_id Varchar(8) primary key
-    course_name Varchar(20) not null
-    dept_name varchar(20) not null
-    credits int not null
-    hour int not null
+    course_id Varchar(8) primary key,
+    course_name Varchar(20) not null,
+    dept_name varchar(20) not null,
+    credits int not null,
+    hour int not null,
     max_seats int not null
 );
 
 create table teacher (
-    teacher_name varchar(100) not null
-    teacher_id varchar(8) primary key
+    teacher_name varchar(100) not null,
+    teacher_id varchar(8) primary key,
     dept_name varchar(20) not null
 );
 
 create table university (
-    semester int not null
-    year int not null
-    primary key (semester, year);
-)
+    semester int not null,
+    year int not null,
+    primary key (semester, year)
+);
 
 create table time (
-    hour int primary key
-    start_time int not null
+    hour int primary key,
+    start_time int not null,
     end_time int not null
-)
+);
 
 create table student (
-    student_id varchar(8) primary key
-    name varchar(100) not null
-    email_id varchar(40) not null
-    dept_name varchar(20) not null
+    student_id varchar(8) primary key,
+    name varchar(100) not null,
+    email_id varchar(40) not null,
+    dept_name varchar(20) not null,
     units int null
-)
+);
